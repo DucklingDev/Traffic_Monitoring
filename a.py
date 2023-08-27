8942138bf4862e86f1c6313932e6ddf06f9fbb3f
noxt1999 <11767593@qq.com>
2023-08-27 13:49:30 +0800

    update

--- a.py
+++ a.py
@@ -2,53 +2,44 @@ import psutil
 import time
 
 def log_total_traffic(logfile, alertfile, hourlyfile):
-    # 获取初始的网络接口信息
     old_net_io = psutil.net_io_counters()
-    
-    # 初始化每小时流量
     gb_sent_hourly = 0
     gb_recv_hourly = 0
     
-    while True:
-        # 获取当前时间
-        current_time = time.localtime()
-        
-        # 等待一秒钟
-        time.sleep(1)
-        
-        # 再次获取网络接口信息
-        new_net_io = psutil.net_io_counters()
-        
-        # 计算发送和接收的字节，并转换为GB
-        gb_sent = (new_net_io.bytes_sent - old_net_io.bytes_sent) / (1024 ** 3)
-        gb_recv = (new_net_io.bytes_recv - old_net_io.bytes_recv) / (1024 ** 3)
-        
-        # 更新每小时的流量
-        gb_sent_hourly += gb_sent
-        gb_recv_hourly += gb_recv
-        
-        # 输出流量情况到控制台
-        print(f'Time: {time.ctime()}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}')
-        
-        # 将结果写入日志文件
-        with open(logfile, 'a') as f:
-            f.write(f'Time: {time.ctime()}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}\n')
-        
-        # 如果流量超过1GB，将结果写入异常流量日志文件
-        if gb_sent > 1 or gb_recv > 1:
-            with open(alertfile, 'a') as f:
-                f.write(f'Time: {time.ctime()}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}\n')
-        
-        # 更新网络接口信息
-        old_net_io = new_net_io
-        
-        # 实时写入每小时流量到日志文件
-        with open(hourlyfile, 'a') as f:
-            if current_time.tm_min == 0 and current_time.tm_sec == 0:
-                f.write(f'Time: {time.ctime()}, Hourly GB sent: {gb_sent_hourly:.6f}, Hourly GB received: {gb_recv_hourly:.6f}\n')
-        
+    # 将文件打开操作移到循环外面
+    with open(logfile, 'a') as logf, open(alertfile, 'a') as alertf, open(hourlyfile, 'a') as hourlyf:
+        while True:
+            start_time = time.perf_counter()
+            current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+            
+            new_net_io = psutil.net_io_counters()
+            
+            gb_sent = (new_net_io.bytes_sent - old_net_io.bytes_sent) / (1024 ** 3)
+            gb_recv = (new_net_io.bytes_recv - old_net_io.bytes_recv) / (1024 ** 3)
+            
+            gb_sent_hourly += gb_sent
+            gb_recv_hourly += gb_recv
+            
+            print(f'Time: {current_time}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}')
+            
+            # 只在需要的时候写入日志
+            logf.write(f'Time: {current_time}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}\n')
+            logf.flush()
+            
+            if gb_sent > 1 or gb_recv > 1:
+                alertf.write(f'Time: {current_time}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}\n')
+                alertf.flush()
+            
+            old_net_io = new_net_io
+            
+            if int(time.strftime("%M")) == 0 and int(time.strftime("%S")) == 0:
+                hourlyf.write(f'Time: {current_time}, Hourly GB sent: {gb_sent_hourly:.6f}, Hourly GB received: {gb_recv_hourly:.6f}\n')
+                hourlyf.flush()
+            
+            # 使用更精确的时间间隔
+            end_time = time.perf_counter()
+            time.sleep(max(1.0 - (end_time - start_time), 0))
 
-# 调用函数
 log_file = "total_traffic_gb.log"
 alert_file = "alert_traffic_gb.log"
 hourly_file = "hourly_traffic_gb.log"
