5dc341368a33fada43e193b2eca9e057b9d2385f
noxt1999 <11767593@qq.com>
2023-08-27 14:24:34 +0800

    update

--- a.py
+++ a.py
@@ -3,10 +3,11 @@ import time
 
 def log_total_traffic(logfile, alertfile, hourlyfile):
     old_net_io = psutil.net_io_counters()
+    gb_sent_minute = 0
+    gb_recv_minute = 0
     gb_sent_hourly = 0
     gb_recv_hourly = 0
     
-    # 将文件打开操作移到循环外面
     with open(logfile, 'a') as logf, open(alertfile, 'a') as alertf, open(hourlyfile, 'a') as hourlyf:
         while True:
             start_time = time.perf_counter()
@@ -17,26 +18,33 @@ def log_total_traffic(logfile, alertfile, hourlyfile):
             gb_sent = (new_net_io.bytes_sent - old_net_io.bytes_sent) / (1024 ** 3)
             gb_recv = (new_net_io.bytes_recv - old_net_io.bytes_recv) / (1024 ** 3)
             
+            gb_sent_minute += gb_sent
+            gb_recv_minute += gb_recv
             gb_sent_hourly += gb_sent
             gb_recv_hourly += gb_recv
             
             print(f'Time: {current_time}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}')
             
-            # 只在需要的时候写入日志
-            logf.write(f'Time: {current_time}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}\n')
-            logf.flush()
+            # 每分钟写入这一分钟内的所有流量总和
+            if int(time.strftime("%S")) == 59:
+                logf.write(f'Time: {current_time}, GB sent: {gb_sent_minute:.6f}, GB received: {gb_recv_minute:.6f}\n')
+                logf.flush()
+                gb_sent_minute = 0
+                gb_recv_minute = 0
             
-            if gb_sent > 1 or gb_recv > 1:
+            # 流量超过500MB就写入
+            if gb_sent > 0.5 or gb_recv > 0.5:
                 alertf.write(f'Time: {current_time}, GB sent: {gb_sent:.6f}, GB received: {gb_recv:.6f}\n')
                 alertf.flush()
             
             old_net_io = new_net_io
             
-            if int(time.strftime("%M")) == 0 and int(time.strftime("%S")) == 0:
+            if int(time.strftime("%M")) == 59 and int(time.strftime("%S")) == 59:
                 hourlyf.write(f'Time: {current_time}, Hourly GB sent: {gb_sent_hourly:.6f}, Hourly GB received: {gb_recv_hourly:.6f}\n')
                 hourlyf.flush()
+                gb_sent_hourly = 0
+                gb_recv_hourly = 0
             
-            # 使用更精确的时间间隔
             end_time = time.perf_counter()
             time.sleep(max(1.0 - (end_time - start_time), 0))
 
